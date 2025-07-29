ea7c5bb46afdbffa414dcf39af82412a475f9c06
Q-Yearn <2434207845@qq.com>
2025-07-29 10:57:24 +0800

    solution 14

--- 最大子数组和/main.py
+++ 最大子数组和/main.py
@@ -10,31 +10,29 @@ class Solution:
         return maxest
 
 # 分治
-from typing import List
-
 class Status:
-    def __init__(self, lSum, rSum, mSum, iSum):
-        self.lSum = lSum  # 左子段最大和
-        self.rSum = rSum  # 右子段最大和
-        self.mSum = mSum  # 最大子段和
-        self.iSum = iSum  # 区间总和
+    def __init__(self, lsum, rsum, msum, isum):
+        self.lsum = lsum
+        self.rsum = rsum
+        self.msum = msum
+        self.isum = isum
 
-class Solution:
-    def pushUp(self, l: Status, r: Status) -> Status:
-        iSum = l.iSum + r.iSum
-        lSum = max(l.lSum, l.iSum + r.lSum)
-        rSum = max(r.rSum, r.iSum + l.rSum)
-        mSum = max(max(l.mSum, r.mSum), l.rSum + r.lSum)
-        return Status(lSum, rSum, mSum, iSum)
+class Solution(object):
+    def merge(self, l, r):
+        isum = l.isum + r.isum
+        lsum = max(l.lsum, l.isum + r.lsum)
+        rsum = max(r.rsum, r.isum + l.rsum)
+        msum = max(max(l.msum, r.msum), l.rsum + r.lsum)  # ← 关键修正
+        return Status(lsum, rsum, msum, isum)
 
-    def get(self, a: List[int], l: int, r: int) -> Status:
+    def get(self, nums, l, r):
         if l == r:
-            return Status(a[l], a[l], a[l], a[l])
+            return Status(nums[l], nums[l], nums[l], nums[l])
         m = (l + r) // 2
-        lSub = self.get(a, l, m)
-        rSub = self.get(a, m + 1, r)
-        return self.pushUp(lSub, rSub)
+        lstatus = self.get(nums, l, m)
+        rstatus = self.get(nums, m + 1, r)
+        return self.merge(lstatus, rstatus)
 
-    def maxSubArray(self, nums: List[int]) -> int:
-        return self.get(nums, 0, len(nums) - 1).mSum
+    def maxSubArray(self, nums):
+        return self.get(nums, 0, len(nums) - 1).msum
 
