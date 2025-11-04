c90e1dfecb43ce11e209ab1874e2af007747151b
Q-Yearn <2434207845@qq.com>
2025-11-04 11:32:41 +0800

    solution 67

---
+++ 寻找旋转排序数组中的最小值/main.py
@@ -0,0 +1,7 @@
+# 理解下面的不行
+# while left < right:
+#     mid = (left + right) // 2
+#     if nums[mid] < nums[left]:
+#         right = mid - 1
+#     else:
+#         left = mid
---
+++ 寻找旋转排序数组中的最小值/my.py
@@ -0,0 +1,16 @@
+# 搜索旋转排序数组中的k值求法
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        return nums[left]
